308cbd86b5d3534eb896f03203da442e5287bac5
Jaquelin-tech <gonzalezcarrillojaquelin@gmail.com>
2024-02-29 12:32:56 -0600

    elementos CSS

--- JAVASCRIPT/functions.js
+++ JAVASCRIPT/functions.js
@@ -1,3 +1,9 @@
 function changeColor(){
-  document.getElementById("palabrita").style.backgroundColor="green";
+   document.getElementById("palabrita").style.backgroundColor="green";
+
+ document.getElementById("palabrita").textContent="Hola";
+}
+
+function enviarMsg(){
+alert("Hola amigo");
 }
